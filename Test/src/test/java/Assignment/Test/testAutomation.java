e1cb4b315a7f3f935fe1dc2f8c985c9e9c7b3bd5
Viral1707 <vpujara7@gmail.com>
2020-03-05 18:34:52 +0530

    Commit

--- Test/src/test/java/Assignment/Test/testAutomation.java
+++ Test/src/test/java/Assignment/Test/testAutomation.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.sikuli.api.Screen;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,35 +19,41 @@ import pageElements.UniversityDepartment;
 public class testAutomation extends base{
 
 	@Test
-	public void testOne() throws IOException, InterruptedException {
+	public void testOne() throws IOException, InterruptedException, FindFailed {
 		
-		initializeBrowser();
+		initializeBrowser(); // Browser Initializer
 		driver.get(prop.getProperty("url"));
 		//driver.manage().window().maximize();
 		Thread.sleep(10000);
-		Homepage hp = new Homepage(driver);
-		JavascriptExecutor executor = (JavascriptExecutor)driver;
+		Homepage hp = new Homepage(driver); // Homepage object creation
+		JavascriptExecutor executor = (JavascriptExecutor)driver; // Used javasript executed to click on department link
 		//long start_time = System.currentTimeMillis();
 		executor.executeScript("arguments[0].click();", hp.getDepartmentLink());
-		UniversityDepartment ud = new UniversityDepartment(driver);
-		Actions action = new Actions(driver);
+		UniversityDepartment ud = new UniversityDepartment(driver); // University Department object creation
+		Actions action = new Actions(driver);					// Actions class to perform hoverover
 		action.moveToElement(ud.getCivilEngineeringLink()).build().perform();
 		Thread.sleep(10000);
-		
-		//Screen s = new Screen();
-		
-		
+		/*
+		// Sikuli is implemented to click on the dropdown tab for which element is being tough to find - but facing challenges with sikuly as well for this
+		Screen screen = new Screen();
+		Pattern linkIOM = new Pattern("E:\\Screenshots" + "CoastalPollutionMonitoringAndHazards.PNG");
+		Pattern linkResearchTheamsCPMH = new Pattern("E:\\Screenshots" + "InstituteforOceanManagement");
+
+		screen.click(linkIOM);
+		*/
 		String actualTitleForOceanManagement = driver.getTitle();
 		//System.out.println(actualTitleForOceanManagement);
 		String expectedTitleForOceanManagement = "Institute For Ocean Management - Anna University offers M.Tech in Coastal Management. ENVIS Center for Coastal Zone Management and Coastal Shelterbelts";
-		Assert.assertEquals(actualTitleForOceanManagement, expectedTitleForOceanManagement);
+		Assert.assertEquals(actualTitleForOceanManagement, expectedTitleForOceanManagement); //Verification of title for page after clicking Institute for Ocean Management
 		InstituteForOceanManagement ifom = new InstituteForOceanManagement(driver);
 		action.moveToElement(ifom.getResearchTheamsLink()).build().perform();
 		Thread.sleep(10000);
+
+		//screen.click(linkResearchTheamsCPMH);
 		String actualTitleForCPMAH = driver.getTitle();
 		//System.out.println(actualTitleForCPMAH);
 		String expectedTitleForCPMAH = ":: IOM - Institute For Ocean Management - Anna University ::";
-		Assert.assertEquals(actualTitleForCPMAH, expectedTitleForCPMAH);
+		Assert.assertEquals(actualTitleForCPMAH, expectedTitleForCPMAH); //Verification of title for page after clicking Costal Pollution Monitoring and Hazards
 		driver.quit();
 		
 	}
